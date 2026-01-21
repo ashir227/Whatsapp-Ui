58b61c2414c2c5842f086012fd219565f364fab4
Ashir Ahmed <ashirahmed810@gmail.com>
2026-01-21 21:32:36 +0500

    add button

--- lib/login/loginscr.dart
+++ lib/login/loginscr.dart
@@ -113,6 +113,7 @@ class _loginscrnState extends State<loginscrn> {
         },
         buttonName: "Next",
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
