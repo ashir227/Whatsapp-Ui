82d44b9670c281a81f86c4cb6922f603448d989f
Ashir Ahmed <ashirahmed810@gmail.com>
2026-01-27 21:47:51 +0500

    Otp scr

--- lib/Screens/Otp_scr.dart
+++ lib/Screens/Otp_scr.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsap/UIhelper/uihelp.dart';
 
 class Otp extends StatefulWidget {
   const Otp({super.key});
@@ -10,6 +11,20 @@ class Otp extends StatefulWidget {
 class _OtpState extends State<Otp> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 45),
+            UIhelper.customtext(
+              text: "Verifying your number",
+              height: 20,
+              color: Color(0xff00A884),
+              fontweight: FontWeight.w700,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
